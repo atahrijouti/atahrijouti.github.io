0f9b6393e2aec15d9138a968dccf35b69c341ecf
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2019-01-10 15:53:42 +0100

    update gh-pages with new version of the site

---
+++ precache-manifest.113f92a34b16f5da3d358477390b8692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab7453f11bc777b2f661",
+    "url": "/static/css/main.5ddfc324.chunk.css"
+  },
+  {
+    "revision": "ab7453f11bc777b2f661",
+    "url": "/static/js/main.ab7453f1.chunk.js"
+  },
+  {
+    "revision": "4d8d1628e49437217a91",
+    "url": "/static/css/1.33898bc8.chunk.css"
+  },
+  {
+    "revision": "4d8d1628e49437217a91",
+    "url": "/static/js/1.4d8d1628.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2f91f8054731f589e8aea0f41f600cf2",
+    "url": "/index.html"
+  }
+];
