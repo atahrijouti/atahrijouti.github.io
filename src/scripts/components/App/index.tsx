2145d9a123646f1827a274b5d02996af00683f7a
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2019-09-23 22:33:50 +0200

    leaves are fully configurable from javascript

--- src/scripts/components/App/index.tsx
+++ src/scripts/components/App/index.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, useCallback, useMemo, useState } from "react"
+import React, {
+  createContext,
+  CSSProperties,
+  useCallback,
+  useMemo,
+  useState,
+} from "react"
 import classNames from "classnames"
 import { MAX_LEAVES_PER_NODE, Node } from "../Node"
 import "./App.css"
@@ -11,6 +17,13 @@ export const LevelsContext = createContext<LevelsContextType>({
   levelReached: 0,
   updateLevelReached: () => {},
 })
+
+const base = 75
+const topAngle = 70
+const rightAngle = 34
+const leftAngle = 180 - topAngle - rightAngle
+
+const d2r = (degree: number) => (degree * Math.PI) / 180
 
 export function App() {
   const [levelReached, setLevel] = useState(0)
@@ -30,10 +43,26 @@ export function App() {
     ]
   }, [])
 
+  const config = useMemo<CSSProperties>(() => {
+    // base / sin(topAngle) = leftBase / sin(rightAngle) = rightBase / sin(leftAngle)
+    const d = base / Math.sin(d2r(topAngle))
+    const rightScale = (d * Math.sin(d2r(leftAngle))) / base
+    const leftScale = (d * Math.sin(d2r(rightAngle))) / base
+
+    return {
+      "--base": `${base}px`,
+      "--right-scale": rightScale,
+      "--left-scale": leftScale,
+      "--top-angle": `${topAngle}deg`,
+      "--right-angle": `${rightAngle}deg`,
+      "--left-angle": `${leftAngle}deg`,
+    } as CSSProperties
+  }, [])
+
   return (
     <LevelsContext.Provider value={{ levelReached, updateLevelReached }}>
       <div className={classNames("App", theme)}>
-        <div className="base">
+        <div className="base" style={config}>
           <Node level={1} id="1" maxChildren={MAX_LEAVES_PER_NODE} />
         </div>
       </div>
