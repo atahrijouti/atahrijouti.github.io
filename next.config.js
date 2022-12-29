fbaf0bbd34065840bf84024d37b3815d859cda10
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2022-12-29 16:46:27 +0100

    update base path

--- next.config.js
+++ next.config.js
@@ -5,12 +5,27 @@
 /**
  * @type {import('next').NextConfig}
  **/
-const config = {
-  basePath: "",
+let config = {
+  // basePath: '/gh-pages-test',
   // assetPrefix: isProd ? '/your-github-repo-name/' : '',
   images: {
     unoptimized: true,
   },
 }
+
+/**
+ * Github pages
+ */
+if (process.env.GITHUB_REPOSITORY) {
+  const repositoryName = process.env.GITHUB_REPOSITORY.split("/")[1]
+
+  config = {
+    ...config,
+    assetPrefix: `/${repositoryName}/`,
+    basePath: `/${repositoryName}`,
+  }
+}
+
+console.log(config)
 
 module.exports = config
