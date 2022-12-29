4872ef52900209f1d3e90c48b4631f881c3eab06
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2022-12-29 14:26:04 +0100

    serve old code from within next

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,122 @@
-import Link from 'next/link'
+import { useEffect } from "react"
+import { runOldJSCode } from "../scripts/script"
 
 export default function Home() {
+  useEffect(() => {
+    runOldJSCode()
+  })
   return (
-    <div>
-      Hello World. <Link href="/about">About</Link>
+    <div className="App">
+      <div id="base" className="base">
+        <div className="Node Node--full" id="1">
+          <div className="Node--inner">
+            <div className="Node Node--even-level Node--full Node--left" id="1-2">
+              <div className="Node--inner">
+                <div className="Node Node--full Node--left" id="1-2-2">
+                  <div className="Node--inner">
+                    <div className="Node Node--even-level Node--full Node--left" id="1-2-2-2">
+                      <div className="Node--inner">
+                        <div className="Node Node--left" id="1-2-2-2-2">
+                          <div className="Node--inner"></div>
+                        </div>
+                        <div className="Node Node--right" id="1-2-2-2-1">
+                          <div className="Node--inner"></div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="Node Node--even-level Node--full Node--right" id="1-2-2-1">
+                      <div className="Node--inner">
+                        <div className="Node Node--left" id="1-2-2-1-2">
+                          <div className="Node--inner"></div>
+                        </div>
+                        <div className="Node Node--right" id="1-2-2-1-1">
+                          <div className="Node--inner"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="Node Node--full Node--right" id="1-2-1">
+                  <div className="Node--inner">
+                    <div className="Node Node--even-level Node--full Node--left" id="1-2-1-2">
+                      <div className="Node--inner">
+                        <div className="Node Node--left" id="1-2-1-2-2">
+                          <div className="Node--inner"></div>
+                        </div>
+                        <div className="Node Node--right" id="1-2-1-2-1">
+                          <div className="Node--inner"></div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="Node Node--even-level Node--full Node--right" id="1-2-1-1">
+                      <div className="Node--inner">
+                        <div className="Node Node--left" id="1-2-1-1-2">
+                          <div className="Node--inner"></div>
+                        </div>
+                        <div className="Node Node--right" id="1-2-1-1-1">
+                          <div className="Node--inner"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="Node Node--even-level Node--full Node--right" id="1-1">
+              <div className="Node--inner">
+                <div className="Node Node--full Node--right" id="1-1-1">
+                  <div className="Node--inner">
+                    <div className="Node Node--even-level Node--full Node--right" id="1-1-1-1">
+                      <div className="Node--inner">
+                        <div className="Node Node--left" id="1-1-1-1-2">
+                          <div className="Node--inner"></div>
+                        </div>
+                        <div className="Node Node--right" id="1-1-1-1-1">
+                          <div className="Node--inner"></div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="Node Node--even-level Node--full Node--left" id="1-1-1-2">
+                      <div className="Node--inner">
+                        <div className="Node Node--left" id="1-1-1-2-2">
+                          <div className="Node--inner"></div>
+                        </div>
+                        <div className="Node Node--right" id="1-1-1-2-1">
+                          <div className="Node--inner"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="Node Node--full Node--left" id="1-1-2">
+                  <div className="Node--inner">
+                    <div className="Node Node--even-level Node--full Node--left" id="1-1-2-2">
+                      <div className="Node--inner">
+                        <div className="Node Node--right" id="1-1-2-2-1">
+                          <div className="Node--inner"></div>
+                        </div>
+                        <div className="Node Node--left" id="1-1-2-2-2">
+                          <div className="Node--inner"></div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="Node Node--even-level Node--full Node--right" id="1-1-2-1">
+                      <div className="Node--inner">
+                        <div className="Node Node--left" id="1-1-2-1-2">
+                          <div className="Node--inner"></div>
+                        </div>
+                        <div className="Node Node--right" id="1-1-2-1-1">
+                          <div className="Node--inner"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ scripts/script.ts
@@ -0,0 +1,67 @@
+import _throttle from "lodash/throttle"
+import _sample from "lodash/sample"
+
+import { computeFractalFromMouse } from "./math"
+import exp from "constants"
+
+let debugElement: HTMLElement | null = null
+let baseElement: HTMLElement | null = null
+let basePosition: DOMRect | undefined = {} as DOMRect
+const relativeWidth = 12.5
+let absoluteWidth: number = 75
+
+const themeList = ["green", "red", "purple"]
+let currentTheme = themeList[0]
+
+const handleMouseMove = _throttle((e: MouseEvent) => {
+  if (!basePosition) {
+    return false
+  }
+  const { rightScale, leftScale, topAngle, rightAngle, leftAngle } = computeFractalFromMouse(
+    e.pageX,
+    e.pageY,
+    basePosition,
+    absoluteWidth,
+  )
+
+  const computedStyle = `
+--base: ${absoluteWidth}px;
+--right-scale: ${rightScale};
+--left-scale: ${leftScale};
+--top-angle: ${topAngle}deg;
+--right-angle: ${rightAngle}deg;
+--left-angle: ${leftAngle}deg;`
+
+  baseElement!.setAttribute("style", computedStyle)
+}, 60)
+
+const handleResize = () => {
+  basePosition = baseElement?.getBoundingClientRect()
+  // absoluteWidth = (window.innerHeight * relativeWidth) / 100
+}
+
+const animateTheme = (app: Element) => {
+  const theme = _sample(themeList) ?? "green"
+  app.classList.remove(`${currentTheme}-theme`)
+  app.classList.add(`${theme}-theme`)
+  currentTheme = theme
+  setTimeout(() => animateTheme(app), 2000)
+}
+
+export function runOldJSCode() {
+  const app = document.querySelector(".App")
+  debugElement = document.getElementById("debug")
+  baseElement = document.getElementById("base")
+
+  if (!baseElement || !app) {
+    console.error({ app, baseElement })
+    return new Error("required elements missing!")
+  }
+
+  basePosition = baseElement.getBoundingClientRect()
+
+  animateTheme(app)
+  handleResize()
+  window.addEventListener("mousemove", handleMouseMove, false)
+  window.addEventListener("resize", handleResize, false)
+}
