bc27082af197e6f860d2a9e07fe7331782ae4f5d
abderrahmane-tj <abderrahmane-tj@users.noreply.github.com>
2023-01-02 13:26:35 +0000

    deploy: 80bd12777b1a349a39903b543b9fbdd3b58dea12

---
+++ _next/static/VTRXKXJPc1dQ6JywPgFNd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d82353691ebde696.js"],"/_error":["static/chunks/pages/_error-82a61e6508dae5a8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
