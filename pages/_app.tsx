642debca727f7f09bcd53a2cf232e342fed5ec42
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2023-01-02 17:17:56 +0100

    src/ folder & console warnings

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from "next/app"
-
-import "../styles/App.css"
-import "../styles/Node.css"
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,10 +6,7 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <link rel="shortcut icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
-        <meta name="theme-color" content="#000000" />
         <link rel="manifest" href="/manifest.webmanifest" />
-        <title>Abderrahmane TAHRI JOUTI</title>
       </Head>
       <body>
         <Main />
