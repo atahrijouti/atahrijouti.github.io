dd127b52ca053949a4e5f8fdd743a24fcb276b57
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2023-01-03 10:14:09 +0100

    start splitting things up

---
+++ src/app/Node.tsx
@@ -0,0 +1,62 @@
+import { CSSProperties, useEffect, useMemo, useState } from "react"
+import _random from "lodash/random"
+import classNames from "classnames"
+
+import "./Node.css"
+
+const MAX_LEVEL = 6
+
+type NodeProps = {
+  orientation?: "left" | "right"
+  level?: number
+}
+
+export const Node = ({ orientation, level = 0 }: NodeProps) => {
+  const [children, setChildren] = useState<NodeProps["orientation"][]>([])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const levelCoefficient = useMemo(() => Math.pow(0.8, level), [])
+  const id = useMemo(() => {
+    if (level === 0) {
+      return "root-node"
+    } else {
+      return `Node-${level}-${orientation == "left" ? 0 : 1}`
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  useEffect(() => {
+    // console.log(`#${id} Effect : Mounted`)
+    if (level < MAX_LEVEL) {
+      const fireTime = _random(333, 666)
+      setTimeout(() => {
+        // console.log(`#${id} setState : Right`)
+        setChildren(["right"])
+      }, fireTime)
+      setTimeout(() => {
+        // console.log(`#${id} setState : Left`)
+        setChildren(["left", "right"])
+      }, _random(fireTime, 999))
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  // console.log(`#${id} Rendered`)
+  return (
+    <div
+      id={id}
+      style={{ "--coefficient": levelCoefficient } as CSSProperties}
+      className={classNames(
+        "Node",
+        {
+          "Node--left": orientation == "left",
+          "Node--right": orientation == "right",
+        },
+        children.length < 2 ? "Node--growing" : "Node--full",
+      )}
+    >
+      <div className="Node--inner">
+        {children.map((orientation) => (
+          <Node key={orientation} orientation={orientation} level={level + 1} />
+        ))}
+      </div>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,75 +1,17 @@
 "use client"
 
-import { CSSProperties, useEffect, useMemo, useState } from "react"
-import classNames from "classnames"
-import _random from "lodash/random"
+import { useEffect } from "react"
 
 import "./App.css"
-import "./Node.css"
 
 import { runOldJSCode } from "app/script"
+import { Node } from "./Node"
 
-const MAX_LEVEL = 6
-
-type NodeProps = {
-  orientation?: "left" | "right"
-  level?: number
-}
-
-const Node = ({ orientation, level = 0 }: NodeProps) => {
-  const [children, setChildren] = useState<NodeProps["orientation"][]>([])
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const levelCoefficient = useMemo(() => Math.pow(0.8, level), [])
-  const id = useMemo(() => {
-    if (level === 0) {
-      return "root-node"
-    } else {
-      return `Node-${level}-${orientation == "left" ? 0 : 1}`
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-  useEffect(() => {
-    // console.log(`#${id} Effect : Mounted`)
-    if (level < MAX_LEVEL) {
-      const fireTime = _random(333, 666)
-      setTimeout(() => {
-        // console.log(`#${id} setState : Right`)
-        setChildren(["right"])
-      }, fireTime)
-      setTimeout(() => {
-        // console.log(`#${id} setState : Left`)
-        setChildren(["left", "right"])
-      }, _random(fireTime, 999))
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-  // console.log(`#${id} Rendered`)
-  return (
-    <div
-      id={id}
-      style={{ "--coefficient": levelCoefficient } as CSSProperties}
-      className={classNames(
-        "Node",
-        {
-          "Node--left": orientation == "left",
-          "Node--right": orientation == "right",
-        },
-        children.length < 2 ? "Node--growing" : "Node--full",
-      )}
-    >
-      <div className="Node--inner">
-        {children.map((orientation) => (
-          <Node key={orientation} orientation={orientation} level={level + 1} />
-        ))}
-      </div>
-    </div>
-  )
-}
-
-export default function Home() {
+const Page = () => {
   useEffect(() => {
     runOldJSCode()
   })
+
   return (
     <div className="App">
       <div id="base" className="base">
@@ -78,3 +20,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Page
