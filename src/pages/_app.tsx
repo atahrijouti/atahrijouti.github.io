f154b22926f410a5b72194f939adb1b570e5c66c
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2023-01-04 18:03:35 +0100

    remove scripts route

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,7 @@ import type { AppProps } from "next/app"
 
 import Head from "next/head"
 
-import "./styles"
-import GlobalStyles from "./styles"
+import GlobalStyles from "../styles"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
---
+++ src/pages/hello.tsx
@@ -0,0 +1,5 @@
+const Hello = () => {
+  return <p>Hello sneaky visitor</p>
+}
+
+export default Hello
