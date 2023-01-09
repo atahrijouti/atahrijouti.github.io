b7ee4431b739f45393d51c02739cf0a05b3fb0aa
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2023-01-09 15:40:39 +0100

    treat __next separately from the page's content

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <title>Abderrahmane TAHRI JOUTI</title>
         <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
       </Head>
-      <Component {...pageProps} />
+      <div className="page">
+        <Component {...pageProps} />
+      </div>
     </>
   )
 }
