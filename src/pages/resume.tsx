ffbb3f1da8908f1f10d4b5acb2d375ef4314564d
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-04-16 13:01:54 +0200

    make a paragraph and disable eslint error

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,8 +1,6 @@
 const Resume = () => {
   return (
-    <>
-      <p>{"A summary of things I've done. That's what Resumé means. Summary"}</p>
-    </>
+    <p>A summary of things I've done. That's what Resumé means. Summary</p>
   )
 }
 
