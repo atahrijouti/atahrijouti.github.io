2bb60c44589842b74e87de0a82ae8dff58505fc0
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-04-28 20:49:28 +0200

    make home page handle its padding on itsown

--- src/page-components/home-page/home-page.css.ts
+++ src/page-components/home-page/home-page.css.ts
@@ -1,7 +1,25 @@
-import { style } from "@vanilla-extract/css"
+import { globalStyle, style } from "@vanilla-extract/css"
+import { layout, menu } from "@/page-components/_layout/site-layout.css"
 
 export const homePage = style({
   display: "flex",
   height: "100%",
   justifyContent: "center",
+})
+
+globalStyle(`${layout}:has(${homePage})`, {
+  paddingLeft: 0,
+  paddingRight: 0,
+})
+
+globalStyle(`${layout}:has(${homePage}) ${menu}`, {
+  paddingLeft: "calc(var(--spacing) * 1.5)",
+  paddingRight: "calc(var(--spacing) * 3)",
+
+  "@media": {
+    "screen and (max-width: 520px)": {
+      paddingLeft: "var(--spacing)",
+      paddingRight: "var(--spacing)",
+    },
+  },
 })
