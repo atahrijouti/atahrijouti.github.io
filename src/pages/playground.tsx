0b13f3b109f2be03aaeead7f32acec4b24c05928
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2023-05-01 17:03:51 +0200

    add a default meta description, and per page titles/descs

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -1,5 +1,21 @@
+import Head from "next/head"
+
 const Playground = () => {
-  return <p>This is a place where I showcase all my playthings</p>
+  return (
+    <>
+      <Head>
+        <title>Playground</title>
+        <meta
+          name="description"
+          content="A sandbox to play with the toys I have made, this is my playground and yours"
+        />
+      </Head>
+      <div>
+        <h1>Playground</h1>
+        <p>This is a place where I showcase all my playthings</p>
+      </div>
+    </>
+  )
 }
 
 export default Playground
