5370c4cc2bfc64b7745098d3a231b1d5a84226ec
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2025-03-01 21:00:41 +0100

    import home util files

---
+++ src/app/home/utils/colors.ts
@@ -0,0 +1,22 @@
+import { COLORS, type Rgb, type RgbNumbers } from "./constants.js"
+
+const { leafyGreen, mudPurple, pinkRed } = COLORS
+
+export const RgbLeafColors = {
+  leafyGreen,
+  pinkRed,
+  mudPurple,
+} as const
+
+type ColorNames = keyof typeof RgbLeafColors
+
+export const LeafColorNumbers = (() => {
+  const entries = Object.entries(RgbLeafColors) as [ColorNames, Rgb][]
+  return entries.reduce(
+    (acc, [colorName, rgbValue]) => {
+      acc[colorName] = rgbValue.replace(/\)|rgb\(|\s/g, "").trim() as RgbNumbers
+      return acc
+    },
+    {} as Record<keyof typeof RgbLeafColors, RgbNumbers>,
+  )
+})()
