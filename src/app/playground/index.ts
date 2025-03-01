0f435d047df1d4d207b9208296742df1736bada2
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2025-03-01 20:56:32 +0100

    add playground page

--- src/app/playground/index.ts
+++ src/app/playground/index.ts
@@ -1,7 +1,14 @@
-export const metadata = {
-  title: "Playground is home",
+import type { Metadata } from "../../types.js"
+import { html } from "../../utils/tags.js"
+
+export const metadata: Metadata = {
+  title: "Playground",
+  description: "A sandbox to play with the toys I have made, this is my playground and yours",
 }
 
 export const content = () => {
-  return `Hello Playgroud.`
+  return html`<div>
+    <h1>Playground</h1>
+    <p>This is a place where I showcase all my playthings</p>
+  </div>`
 }
