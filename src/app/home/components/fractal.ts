ddfe96eca9e13e2493fb18f9ca94585883bf7478
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2025-03-02 10:34:26 +0100

    use ASCII code instead of the character itself

--- src/app/home/components/fractal.ts
+++ src/app/home/components/fractal.ts
@@ -4,7 +4,7 @@ export const Fractal = () => {
   // todo: add class hidden to ball inner upon first drag
   return html`<div id="canvas" class="fractal canvas">
     <div class="canvas-inner">
-      <div id="base" class="base">${Leaf()}</div>
+      <div id="base" class="base">{Leaf()}</div>
       <div id="visual-target" class="visual-target">
         <div class="target-ball">
           <div class="ball-inner"></div>
