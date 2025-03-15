4533b8a8de5e78634235efd5ad94bfa30685ee05
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2025-03-15 22:33:43 +0100

    initial fractalization

--- src/app/home/components/fractal.ts
+++ src/app/home/components/fractal.ts
@@ -1,10 +1,21 @@
 import { html } from "../../../utils/tags.js"
 
+type LeafProps = {
+  orientation?: "left" | "right"
+  level?: number
+}
+export const Leaf = ({ level = 0, orientation }: LeafProps = {}) => {
+  const id = level === 0 ? "root-node" : `Node-${level}-${orientation == "right" ? 0 : 1}`
+  const orientationClass = orientation ? (orientation === "right" ? orientation : "left") : ""
+  //
+  return html`<div id="${id}" class="leaf ${orientationClass}"></div>`
+}
+
 export const Fractal = () => {
   // todo: add class hidden to ball inner upon first drag
   return html`<div id="canvas" class="fractal canvas">
     <div class="canvas-inner">
-      <div id="base" class="base">{Leaf()}</div>
+      <div id="base" class="base">${Leaf()}</div>
       <div id="visual-target" class="visual-target">
         <div class="target-ball">
           <div class="ball-inner"></div>
