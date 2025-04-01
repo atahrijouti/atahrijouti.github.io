867215da7dfeb355c1be6a7a24c14da378508fbe
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2025-04-01 14:16:25 +0200

    starting tree sitter

---
+++ src/app/tree-sitter/index.ts
@@ -0,0 +1,17 @@
+import type { Metadata } from "../../types.js"
+import { html } from "../../utils/tags.js"
+
+export const metadata: Metadata = {
+  title: "Tree sitter preview",
+  description: "Preview your tree-sitter theme",
+}
+
+export const ready = async () => {
+  console.log("tree sitter things")
+}
+
+export const content = () => {
+  return html`<div class="tree-sitter-page">
+    <h2>tree-sitter</h2>
+  </div>`
+}
